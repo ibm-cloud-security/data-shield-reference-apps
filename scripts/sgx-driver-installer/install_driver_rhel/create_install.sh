97e105b024117a36601074ec23fe3768b950f044
Christina Mak <christina.mak@ibm.com>
2020-02-04 16:24:12 -0600

    Install script for RHEL SGX drivers

---
+++ scripts/sgx-driver-installer/install_driver_rhel/create_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl delete pod sgx-driver-installer
+ibmcloud cr image-rm us.icr.io/datashield-core/sgx-driver-installer
+docker build -t us.icr.io/datashield-core/sgx-driver-installer . 
+docker push us.icr.io/datashield-core/sgx-driver-installer
+kubectl create -f deployment_install_sgx_driver.yaml 
---
+++ scripts/sgx-driver-installer/install_driver_rhel/startup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chroot /host bash < install_sgx_driver.sh
