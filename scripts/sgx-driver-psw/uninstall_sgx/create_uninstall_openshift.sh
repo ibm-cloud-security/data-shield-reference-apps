915858170d0a85eeedbc1c16d1466aa280fba657
Christina Mak <Christina.Mak@ibm.com>
2021-08-06 12:02:32 -0700

    Remove unused files

--- scripts/sgx-driver-psw/uninstall_sgx/create_uninstall_openshift.sh
+++ scripts/sgx-driver-psw/uninstall_sgx/create_uninstall_openshift.sh
@@ -6,12 +6,6 @@ docker build -t us.icr.io/datashield-core/ibm-sgx-uninstaller .
 docker push us.icr.io/datashield-core/ibm-sgx-uninstaller
 oc create -f scc.yaml
 oc create serviceaccount sgx-admin
-# oc secrets add serviceaccount/sgx-admin secrets/default-icr-io --for=pull
-# oc secrets add serviceaccount/sgx-admin secrets/default-us-icr-io --for=pull
-# oc secrets add serviceaccount/sgx-admin secrets/default-uk-icr-io --for=pull
-# oc secrets add serviceaccount/sgx-admin secrets/default-de-icr-io --for=pull
-# oc secrets add serviceaccount/sgx-admin secrets/default-au-icr-io --for=pull
-# oc secrets add serviceaccount/sgx-admin secrets/default-jp-icr-io --for=pull
 oc secrets link sgx-admin all-icr-io --for=pull
 oc adm policy add-scc-to-user sgx-admin -z sgx-admin
 oc create -f deployment_uninstall_sgx_openshift.yaml 
